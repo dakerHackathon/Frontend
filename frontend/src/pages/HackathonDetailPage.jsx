5e1434bf7d5197348b74332f200e556223dc5fd2
김민준 <knn330@naver.com>
2026-03-27 15:38:58 +0900

    style: fixed modal layout bug

--- frontend/src/pages/HackathonDetailPage.jsx
+++ frontend/src/pages/HackathonDetailPage.jsx
@@ -194,13 +194,19 @@ const HackathonDetailPage = () => {
   useEffect(() => {
     const previousBodyOverflow = document.body.style.overflow;
     const previousHtmlOverflow = document.documentElement.style.overflow;
+    const previousBodyPaddingRight = document.body.style.paddingRight;
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
 
     document.body.style.overflow = "hidden";
     document.documentElement.style.overflow = "hidden";
+    if (scrollbarWidth > 0) {
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    }
 
     return () => {
       document.body.style.overflow = previousBodyOverflow;
       document.documentElement.style.overflow = previousHtmlOverflow;
+      document.body.style.paddingRight = previousBodyPaddingRight;
     };
   }, []);
 
