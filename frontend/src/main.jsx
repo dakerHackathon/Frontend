f1cb160da250488bfba35ffdc4d3c3ce43b24165
yi9353 <yi9353@naver.com>
2026-03-31 09:42:51 +0900

    chore: 로그인 로직 수정

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-if (import.meta.env.DEV) {
-  const { worker } = await import("./mocks/browser.js");
+// if (import.meta.env.DEV) {
+//   const { worker } = await import("./mocks/browser.js");
 
-  worker.start({
-    onUnhandledRequest: "bypass", // 정적 파일(JS, CSS)은 무시하고 통과
-  });
-}
+//   worker.start({
+//     onUnhandledRequest: "bypass", // 정적 파일(JS, CSS)은 무시하고 통과
+//   });
+// }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
