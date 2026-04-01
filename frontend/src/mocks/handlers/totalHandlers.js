f0ef575517090cd8ba6d9b92e211cf83a0671c20
김민준 <knn330@naver.com>
2026-04-01 15:50:45 +0900

    refactor: 해커톤 도메인 컴포넌트·목업 데이터·상수·유틸 분리

--- frontend/src/mocks/handlers/totalHandlers.js
+++ frontend/src/mocks/handlers/totalHandlers.js
@@ -1,4 +1,5 @@
+import { hackathonHandlers } from "./hackathonHandlers";
 import { userHandlers } from "./userHandlers";
 
 // 도메인별 핸들러들을 하나로 합칩니다.
-export const totalHandlers = [...userHandlers];
+export const totalHandlers = [...userHandlers, ...hackathonHandlers];
