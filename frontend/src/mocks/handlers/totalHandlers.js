edf1b61d8305327fd37ef8192e305c783ec1fcb0
uzinkkk <yj051888@kumoh.ac.kr>
2026-04-03 17:31:40 +0900

    chore: recharts 패키지 추가

--- frontend/src/mocks/handlers/totalHandlers.js
+++ frontend/src/mocks/handlers/totalHandlers.js
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
 import { mailHandlers } from "./mailHandlers";
 import { mypageHandlers } from "./mypageHandlers";
 import { skillHandlers } from "./skillHandlers";
 import { userHandlers } from "./userHandlers";
-
-// 메일, 마이페이지, 스킬, 유저 핸들러를 모두 합칩니다.
-export const totalHandlers = [
-  ...userHandlers,
-  ...mailHandlers,
-  ...mypageHandlers,
-  ...skillHandlers,
-=======
-import { userHandlers } from "./userHandlers";
 import { hackathonHandlers } from "./hackathonHandlers";
-import { mailHandlers } from "./mailHandlers";
 import { teamHandlers } from "./teamHandlers";
 import { recruitHandlers } from "./recruitHandlers";
 
-// 도메인별 핸들러들을 하나로 합칩니다.
 export const totalHandlers = [
   ...userHandlers,
   ...hackathonHandlers,
   ...mailHandlers,
+  ...mypageHandlers,
+  ...skillHandlers,
   ...teamHandlers,
   ...recruitHandlers,
->>>>>>> origin/main
 ];
