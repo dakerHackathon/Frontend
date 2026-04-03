17d346c4a1d6653f5f5b5df6b6425283fff858bf
김민준 <knn330@naver.com>
2026-04-03 16:27:14 +0900

    fix: 팀 참가 신청 훅 구조 정리

--- frontend/src/api/team.js
+++ frontend/src/api/team.js
@@ -2,4 +2,7 @@ import axiosInstance from "./axiosInstance";
 
 export const teamApi = {
   create: (userId, data) => axiosInstance.post(`/camp/${userId}/team`, data),
+  getLeaderTeams: (userId) => axiosInstance.get(`/camp/${userId}/team`),
+  registerHackathon: (userId, teamId, data) =>
+    axiosInstance.post(`/camp/${userId}/team/${teamId}/register`, data),
 };
--- frontend/src/mocks/handlers/totalHandlers.js
+++ frontend/src/mocks/handlers/totalHandlers.js
@@ -1,6 +1,7 @@
 import { userHandlers } from "./userHandlers";
 import { hackathonHandlers } from "./hackathonHandlers";
 import { mailHandlers } from "./mailHandlers";
+import { teamHandlers } from "./teamHandlers";
 
 // 도메인별 핸들러들을 하나로 합칩니다.
-export const totalHandlers = [...userHandlers, ...hackathonHandlers, ...mailHandlers];
+export const totalHandlers = [...userHandlers, ...hackathonHandlers, ...mailHandlers, ...teamHandlers];
