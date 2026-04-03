6ad573059358214d4dedb6bcf45b8999df78ffc3
김민준 <knn330@naver.com>
2026-04-03 13:46:26 +0900

    fix: 해커톤 API 레지스트리 연결 복구

--- frontend/src/api/api_registry.js
+++ frontend/src/api/api_registry.js
@@ -1,11 +1,13 @@
 //api 紐⑥쓬吏?
 import { authApi } from "./auth";
+import { hackathonApi } from "./hackathon";
 import { mypageApi } from "./mypage";
 import { teamApi } from "./team";
 import { mailApi } from "./mail";
 
 export const API = {
   auth: authApi,
+  hackathon: hackathonApi,
   mypage: mypageApi,
   team: teamApi,
   mail: mailApi,
