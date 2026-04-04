1aba52f0e041ed34f2555f2b9da21b2d14da029b
hodu42 <knn330@naver.com>
2026-04-04 22:07:33 +0900

    chore: 실서버 연동을 위한 API 설정 변경

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5173",
-  //실제 서버 주소 : 13.125.160.175:8080
+  baseURL: "http://13.125.160.175:8080",
+  // 로컬 MSW 확인이 필요할 때만 localhost:5173으로 되돌려서 사용합니다.
   timeout: 3000,
   headers: {
     "Content-Type": "application/json",
